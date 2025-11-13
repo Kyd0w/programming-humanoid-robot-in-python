85d282eaf55df3699386c9f45e67a5a873461757
Kyd0w <kyanhngn@gmail.com>
2025-11-13 13:12:39 +0100

    zwischenabgabge

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -35,14 +35,57 @@ class AngleInterpolationAgent(PIDAgent):
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
-        target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
+        if 'LHipYawPitch' in target_joints:
+            target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
         self.target_joints.update(target_joints)
         return super(AngleInterpolationAgent, self).think(perception)
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        names, times, keys = keyframes
+        if not hasattr(self, "_motionStartTime"):
+            self._motionStartTime = perception.time
+        currentTime = perception.time - self._motionStartTime
+        for k in range(len(names)):
+            joint_name = names[k]
+            jointTimes = times[k]
+            jointKeys = keys[k]
+            if len(jointTimes) == 0:
+                continue
+            if currentTime <= jointTimes[0]:
+                target_joints[joint_name] = jointKeys[0][0]
+                continue
+            if currentTime >= jointTimes[-1]:
+                target_joints[joint_name] = jointKeys[-1][0]
+                continue
+            t0 = t1 = None
+            angle0 = angle1 = None
+            h0_after = h1_before = None
+            for j in range(len(jointTimes)-1):
+                if jointTimes[j] <= currentTime < jointTimes[j+1]:
+                    t0 = jointTimes[j]
+                    t1 = jointTimes[j+1]
 
+                    angle0, h0_before, h0_after = jointKeys[j]
+                    angle1, h1_before, h1_after = jointKeys[j+1]
+                    break
+            if t0 is None:
+                target_joints[joint_name] = jointKeys[-1][0]
+                continue
+            i = (currentTime - t0) / (t1 - t0)
+            p0 = angle0
+            p1 = angle0 + h0_after[2]
+            p2 = angle1 + h1_before[2]
+            p3 = angle1
+            angle = (
+                (1-i)**3 * p0
+                + 3*(1-i)**2 * i * p1
+                + 3*(1-i) * (i**2) * p2
+                + (i**3) * p3
+            )
+
+            target_joints[joint_name] = angle
         return target_joints
 
 if __name__ == '__main__':
--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 14
+        self.Ki = 0.1
+        self.Kd = 0.2
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,7 +53,18 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
-
+        error = target - sensor
+        deltaU = (
+            (self.Kp + self.Ki *self.dt + (self.Kd/self.dt))
+            * error -(self.Kp + (2*self.Kd/self.dt))* self.e1
+            + (self.Kd/self.dt)*self.e2
+        )
+        self.u = self.u + deltaU
+        e1 = self.e1
+        self.e2 = e1
+        self.e1 = error
+        predictSensor = sensor +self.u* self.dt
+        self.y.append(predictSensor)
         return self.u
 
 
